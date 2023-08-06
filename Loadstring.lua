7b3bf78185543ab883a81ae7b83fbc9537f72b55
SubnauticaLaserMain <123651736+SubnauticaLaserMain@users.noreply.github.com>
2023-08-06 19:29:25 +0200

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,29 @@
+if game.PlaceId == 3851622790 or game.PlaceId == 4620170611 then
+	if game:IsLoaded() then
+		warn("[ServerScriptAPI]: Break in (Story) game has Loaded!")
+	end
+	if not game:IsLoaded() then
+		error("[ServerScriptAPI]: Break in (Story) hasent Loaded. Pls wait until its loaded")
+	end
+end
 
+if game.PlaceId == 286090429 then
+	if game:IsLoaded() then
+		warn("[ServerScriptAPI]: Arsenal has Loaded!")	
+	end
+	if not game:IsLoaded() then
+		error("[ServerScriptAPI]: Arsenal hasent Loaded. Pls wait until its loaded")
+	end
+end
+
+if game.PlaceId == 286090429 then
+	if game:IsLoaded() then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/SubnauticaLaserMain/script_2/main/286090429.lua", true))()
+	end
+end
+
+if game.PlaceId == 3851622790 or game.PlaceId == 4620170611 then
+	if game:IsLoaded() then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/SubnauticaLaserMain/script_2/main/Break%20in%20Story%20Script.lua", true))()
+	end
+end
